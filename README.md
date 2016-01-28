e3ad41d6accb1ab32bb30b49697f2211780d7277
John Rachuy II <john.rachuy@gmail.com>
2016-01-27 23:03:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# base_project
+#
+# @author John Rachuy II
+# @version 0.0.1 01/27/2016
+
---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("It all starts here!");
+
+//@author John Rachuy II
+//@version 0.0.1 01/27/2016
